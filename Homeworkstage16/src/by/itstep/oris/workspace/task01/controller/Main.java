aee8ad3f505125f170e569b0dfddef8d15b31716
User <User@User-PC>
2024-03-03 12:23:03 +0300

    Add controller Main class task 1

---
+++ Homeworkstage16/src/by/itstep/oris/workspace/task01/controller/Main.java
@@ -0,0 +1,16 @@
+package by.itstep.oris.workspace.task01.controller;
+
+import by.itstep.oris.workspace.task01.logic.MatrixManager;
+
+public class Main {
+	public static void main(String[] args) {
+		int[][] matrix = { 
+				{ 1, -5, 3 }, 
+				{ -1, 2, 3 }, 
+				{ 3, -5, 2 } };
+
+		int sum = MatrixManager.getSumBetweenPositives(matrix);
+
+		System.out.print(sum);
+	}
+}
