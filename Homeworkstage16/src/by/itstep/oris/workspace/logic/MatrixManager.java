a466cb9b9b4a42157436e412d5ef45732667a9ab
User <User@User-PC>
2024-03-03 12:57:52 +0300

    Add getColumNumbersWithSequence method

--- Homeworkstage16/src/by/itstep/oris/workspace/logic/MatrixManager.java
+++ Homeworkstage16/src/by/itstep/oris/workspace/logic/MatrixManager.java
@@ -3,25 +3,55 @@ package by.itstep.oris.workspace.logic;
 public class MatrixManager {
 	public static int getSumBetweenPositives(int[][] matrix) {
 		int sum = 0;
-		
-	    for (int[] row : matrix) {
-	        boolean foundFirstPositive = false;
-	        int partialSum = 0;
-	        
-	        for (int num : row) {
-	            if (num > 0) {
-	                if (foundFirstPositive) {
-	                    sum += partialSum;
-	                    break;
-	                } else {
-	                    foundFirstPositive = true;
-	                }
-	            } else if (foundFirstPositive) {
-	                partialSum += num;
-	            }
-	        }
-	    }
-	    
-	    return sum;
+
+		for (int[] row : matrix) {
+			boolean foundFirstPositive = false;
+			int partialSum = 0;
+
+			for (int num : row) {
+				if (num > 0) {
+					if (foundFirstPositive) {
+						sum += partialSum;
+						break;
+					} else {
+						foundFirstPositive = true;
+					}
+				} else if (foundFirstPositive) {
+					partialSum += num;
+				}
+			}
+		}
+
+		return sum;
+	}
+
+	public int[] getColumNumbersWithSequence(int[][] matrix) {
+		int[] columnNumbers = new int[matrix.length];
+
+        for (int columnIndex = 0; columnIndex < matrix[0].length; columnIndex++) {
+            boolean isAscending = true;
+            boolean isDescending = true;
+
+            for (int i = 0; i < matrix.length - 1; i++) {
+                if (matrix[i][columnIndex] > matrix[i + 1][columnIndex]) {
+                    isAscending = false;
+                }
+                if (matrix[i][columnIndex] < matrix[i + 1][columnIndex]) {
+                    isDescending = false;
+                }
+            }
+
+            if (isAscending) {
+                columnNumbers[columnIndex] = 1;
+            } else if (isDescending) {
+                columnNumbers[columnIndex] = -1;
+            } else {
+                columnNumbers[columnIndex] = 0;
+            }
+        }
+
+        return columnNumbers;
     }
+
+
 }
