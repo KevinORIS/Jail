380184fadb44a2e4bdee27c7a16a74d21f4eaf5f
User <User@User-PC>
2024-03-03 22:59:47 +0300

    Add calcTotalTerm method

--- Jail/src/by/itstep/oris/workspace/model/logic/Prison.java
+++ Jail/src/by/itstep/oris/workspace/model/logic/Prison.java
@@ -4,34 +4,51 @@ import by.itstep.oris.workspace.model.entity.Prisoner;
 
 public class Prison {
 	public static double getLongestTerm(Prisoner[] prisoners) {
-		if(prisoners == null || prisoners.length == 0) {
+		if (prisoners == null || prisoners.length == 0) {
 			return 0;
 		}
-		
+
 		double longestTerm = 0;
-		
-		for(Prisoner prisoner : prisoners) {
+
+		for (Prisoner prisoner : prisoners) {
 			if (longestTerm > prisoner.getTerm()) {
 				longestTerm = prisoner.getTerm();
 			}
 		}
-		
+
 		return longestTerm;
 	}
-	
+
 	public static double getShortestTerm(Prisoner[] prisoners) {
-		if(prisoners == null || prisoners.length == 0) {
+		if (prisoners == null || prisoners.length == 0) {
 			return 0;
 		}
-		
+
 		double shortestTerm = 0;
-		
-		for(Prisoner prisoner : prisoners) {
+
+		for (Prisoner prisoner : prisoners) {
 			if (shortestTerm < prisoner.getTerm()) {
 				shortestTerm = prisoner.getTerm();
 			}
 		}
-		
+
 		return shortestTerm;
+	}
+
+	public static double calcTotalTerm(Prisoner[] prisoners) {
+		if (prisoners == null || prisoners.length == 0) {
+			return 0;
+		}
+
+		double termSum = 0;
+
+		for (Prisoner prisoner : prisoners) {
+
+			termSum += prisoner.getTerm();
+
+		}
+
+		return termSum;
+
 	}
 }
