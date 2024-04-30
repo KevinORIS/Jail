6842cacf90d9de7279ce3a12a03ccfa4509467f8
User <User@User-PC>
2024-04-30 15:34:52 +0300

    Add test method

--- Jail/test/by/itstep/oris/workspace/model/logic/PrisonTest.java
+++ Jail/test/by/itstep/oris/workspace/model/logic/PrisonTest.java
@@ -11,11 +11,22 @@ public class PrisonTest {
 	@Test
 	public void testGetLongestTerm() {
 		Prisoner[] prisoners = {new Prisoner("Bob", "Petty theft", 3),
-								new Prisoner("Jack", "Public masturbation", 10)};
-		int expected = 10;
+								new Prisoner("Jack", "Public masturbation", 10),
+								new Prisoner("Andy", "Armed attack", 23)};
+		int expected = 23;
 		int actual = Prison.getLongestTerm(prisoners);
 		
 		assertEquals(expected, actual);
 	}
-
+	
+	@Test
+	public void testGetShortestTerm() {
+		Prisoner[] prisoners = {new Prisoner("Bob", "Petty theft", 3),
+				new Prisoner("Jack", "Public masturbation", 10),
+				new Prisoner("Andy", "Armed attack", 23)};
+		int expected = 3;
+		int actual = Prison.getShortestTerm(prisoners);
+		
+		assertEquals(expected, actual);
+	}
 }
