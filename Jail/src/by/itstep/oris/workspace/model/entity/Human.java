3052dc15e55f64364c53c89ebb8df878420a15fb
User <User@User-PC>
2024-05-01 10:02:40 +0300

    Create class Human

---
+++ Jail/src/by/itstep/oris/workspace/model/entity/Human.java
@@ -0,0 +1,20 @@
+package by.itstep.oris.workspace.model.entity;
+
+public class Human {
+	protected String name;
+
+	public Human() {
+		name = "No name";
+	}
+
+	public Human(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
